c3d3b25e1d1c3ecdb5f5d2eeda652657c7902d74
cloudy1gor <savelyev7979@gmail.com>
2025-04-25 17:24:50 +0300

    FIX:chenged export.sh file

--- export.sh
+++ export.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 _os="`uname`"
 _now=$(date +"%m_%d_%Y")
-_file="wp-data/data_$_now.sql"
+_file="wordpress/data_$_now.sql"
 
 # Export dump
-EXPORT_COMMAND='exec mysqldump "$MYSQL_DATABASE" -uroot -p"$MYSQL_ROOT_PASSWORD"'
+EXPORT_COMMAND='exec mysqldump "$DB_NAME" -uroot -p"$DB_ROOT_PASSWORD"'
 docker-compose exec db sh -c "$EXPORT_COMMAND" > $_file
 
 if [[ $_os == "Darwin"* ]] ; then
